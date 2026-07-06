25bccf958bf17135bd72c56b08d1410ed562dbba
MrFaktum <kirtamal@gmail.com>
2026-07-06 10:16:58 +0300

    Пример расстановки объектов

--- scn/player/mouth.gd
+++ scn/player/mouth.gd
@@ -13,19 +13,14 @@ func _ready():
 func _on_body_entered(body: Node3D):
 	# Проверяем группу
 	if body.is_in_group("Edible"):
-		
 		# Ищем компонент с данными Node food_data
 		var food_data = body.get_node_or_null("FoodData")
-		
 		# Если компонент найден
 		if food_data != null:
-			
 			# 1. Красим крошки в цвет еды
 			crumbs.color = food_data.crumb_color
-			
 			# 2. Выбрасываем крошки (включаем эмиттер)
 			crumbs.emitting = true
-			
 			# 3. Выводим данные
 			print("Съедено! Голод: +", food_data.food_value, " Вода: +", food_data.water_value)
 			if food_data.poison_value > 0:
